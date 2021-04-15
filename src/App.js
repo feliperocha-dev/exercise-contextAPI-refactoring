fb715889601980964017b739a7897bee203e3424
Felipe Rocha <felipe.sr27@gmail.com>
2021-04-15 11:34:09 -0300

    Context API: Exercise One

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './context/Context';
 
-function App() {
-  return (
-    <Cars />
-  );
+const { Provider } = MyContext;
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+        test: false,
+      },
+    };
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(name, side) {
+    console.log(name, side)
+    this.setState((prevState) => ({
+      cars: {
+        ...prevState.cars,
+        [name]: side,
+      },
+    }));
+  }
+
+  render() {
+    const { cars } = this.state;
+    const carsObj = {
+      moveCar : this.moveCar,
+      cars,
+    }
+    return (
+      <Provider value={ carsObj }>
+        <Cars />
+      </Provider>
+    );
+  }
 }
 
 export default App;
